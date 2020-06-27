1452fa661cee69e6b3e709981a87be8e7f630b30
Roman Lavrov <lavrowroman1983@gmail.com>
2020-06-27 13:11:59 +0300

    Order status updated

--- FamilyBrowser/application/views/de/Orders/orderStatus_view.php
+++ FamilyBrowser/application/views/de/Orders/orderStatus_view.php
@@ -21,15 +21,11 @@
             <th class="orderTableHeader">System</th>
             <th class="orderTableHeader">Revit Kategorie</th>
             <th class="orderTableHeader">Beschreibung</th>
-            <th class="orderTableHeader">Installationsart</th>
-            <th class="orderTableHeader">Installationsort</th>
-            <th class="orderTableHeader">Installationsmedium</th>
+            <th class="orderTableHeader">Optionen</th>
             <th class="orderTableHeader">Größe</th>
-            <th class="orderTableHeader">2D Bild</th>
-            <th class="orderTableHeader">3D Bild</th>
-            <th class="orderTableHeader">Spezifikation</th>
+            <th class="orderTableHeader">Datein</th>
             <th class="orderTableHeader">Datum der Bestellung</th>
-            <th class="orderTableHeader">Satus bestellen</th>
+            <th class="orderTableHeader">Status</th>
         </tr>
     </thead>
 
@@ -40,34 +36,70 @@
                 <td style="text-align:center"><?php print htmlentities($order['System']) ?></td>
                 <td style="text-align:center"><?php print htmlentities($order['RevitCategory']) ?></td>
                 <td><?php print htmlentities($order['Description']) ?></td>
-                <td style="text-align:center"><?php print htmlentities($order['Mount']) ?></td>
-                <td style="text-align:center"><?php print htmlentities($order['Placement']) ?></td>
-                <td style="text-align:center"><?php print htmlentities($order['InstallationMedium']) ?></td>
+                <td>
+                    <div class="d-flex flex-row pb-1">
+                        <div class="w-50">Installationsart:</div>
+                        <div class="w-50"> <?php print htmlentities($order['Mount']) ?></div>
+                    </div>
+                    <div class="d-flex flex-row pb-1">
+                        <div class="w-50">Installationsort:</div>
+                        <div class="w-50"><?php print htmlentities($order['Placement']) ?></div>
+                    </div>
+                    <div class="d-flex flex-row pb-1">
+                        <div class="w-50">Installationsmedium:</div>
+                        <div class="w-50"><?php print htmlentities($order['InstallationMedium']) ?></div>
+                    </div>
+                </td>
+
                 <td>
                     <div>
                         <div class="d-flex flex-row">
-                            <div class="p-2">Höhe:</div>
-                            <div class="p-2"><?php print htmlentities($order['Height']) ?></div>
+                            <div class="p-1">Höhe:</div>
+                            <div class="p-1"><?php print htmlentities($order['Height']) ?></div>
                         </div>
                         <div class="d-flex flex-row">
-                            <div class="p-2">Breite:</div>
-                            <div class="p-2"><?php print htmlentities($order['Width']) ?></div>
+                            <div class="p-1">Breite:</div>
+                            <div class="p-1"><?php print htmlentities($order['Width']) ?></div>
                         </div>
                         <div class="d-flex flex-row">
-                            <div class="p-2">Tiefe:</div>
-                            <div class="p-2"><?php print htmlentities($order['Depth']) ?></div>
+                            <div class="p-1">Tiefe:</div>
+                            <div class="p-1"><?php print htmlentities($order['Depth']) ?></div>
                         </div>
                         <div class="d-flex flex-row">
-                            <div class="p-2">Durchmesser:</div>
-                            <div class="p-2"><?php print htmlentities($order['Diameter']) ?></div>
+                            <div class="p-1">Durchmesser:</div>
+                            <div class="p-1"><?php print htmlentities($order['Diameter']) ?></div>
                         </div>
                     </div>
                 </td>
-                <td style="text-align:center"><img class="img-thumbnail rounded " src="<?php print htmlentities('/FamilyBrowser/application/orderFilesUploads/' . $order['File2d']) ?>" alt="No Image"></th>
-                <td style="text-align:center"><img class="img-thumbnail rounded " src="<?php print htmlentities('/FamilyBrowser/application/orderFilesUploads/' . $order['File3d']) ?>" alt="No Image"></th>
-                <td style="text-align:center"><img class="img-thumbnail rounded " src="<?php print htmlentities('/FamilyBrowser/application/orderFilesUploads/' . $order['FileSpecification']) ?>" alt="No Image"></th>
+                <td> <div class="d-flex flex-row">
+                        <div class="p-2 w-50 align-self-center">2D Symbol:</div>
+                        <div class="p-2 w-50 align-self-center">
+                            <img class="img-thumbnail rounded file-preview" src="<?php print htmlentities('/FamilyBrowser/application/orderFilesUploads/' . $order['File2d']) ?>" alt="No Image"   data-link = "<?php  print htmlentities('/FamilyBrowser/application/orderFilesUploads/' . $order['File2d'])?>" >
+                        </div>
+                    </div>
+                    <div class="d-flex flex-row">
+                        <div class="p-2 w-50 align-self-center">3D Symbol:</div>
+                        <div class="p-2 w-50 align-self-center">
+                            <img class="img-thumbnail rounded file-preview" 
+                            src="<?php print htmlentities('/FamilyBrowser/application/orderFilesUploads/' . $order['File3d']) ?>" alt="No Image"  
+                            data-link = "<?php  print htmlentities('/FamilyBrowser/application/orderFilesUploads/' . $order['File3d'])?>">
+                        </div>
+                    </div>
+                    <div class="d-flex flex-row">
+                        <div class="p-2 w-50 align-self-center">Spezifikation</div>
+                        <div class="p-2 w-50 align-self-center">
+                            <img class="img-thumbnail rounded file-preview" 
+                            src="<?php if (strpos($order['FileSpecification'], '.pdf') > 0) {
+                                                                            print htmlentities('/FamilyBrowser/application/orderFilesUploads/logo-pdf.png');
+                                                                        } else {
+                                                                            print htmlentities('/FamilyBrowser/application/orderFilesUploads/' . $order['FileSpecification']);
+                                                                        } ?>" alt="No Image"
+                            data-link = "<?php  print htmlentities('/FamilyBrowser/application/orderFilesUploads/' . $order['FileSpecification'])?>">
+                        </div>
+                    </div></td>
+               
                 <td style="text-align:center"><?php print htmlentities($order['CreatedAt']) ?></td>
-                <td style="text-align:center">Status</td>
+                <td style="text-align:center"><?php print htmlentities($order['StatusId']) ?></td>
             <tr>
             <?php endforeach ?>
     </tbody>
