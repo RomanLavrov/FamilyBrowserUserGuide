fc648a7055fafb06c392496f8ea7e6a38b870105
Roman Lavrov <lavrowroman1983@gmail.com>
2021-12-16 16:31:03 +0200

    Download link is added to the Release notes

--- FamilyBrowser/application/views/en/release_history_view.php
+++ FamilyBrowser/application/views/en/release_history_view.php
@@ -1,4 +1,12 @@
-<div class="article-header">Family Browser 2022.0</div>
+<div class="article-header">
+    <span>Family Browser 2022.0</span>
+    <a href="https://s3.eu-central-1.amazonaws.com/revit.family.browser/Release+Notes/Release-Notes-FB2021.1.pdf">
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-download" viewBox="0 0 16 16">
+            <path d="M.5 9.9a.5.5 0 0 1 .5.5v2.5a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-2.5a.5.5 0 0 1 1 0v2.5a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2v-2.5a.5.5 0 0 1 .5-.5z" />
+            <path d="M7.646 11.854a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293V1.5a.5.5 0 0 0-1 0v8.793L5.354 8.146a.5.5 0 1 0-.708.708l3 3z" />
+        </svg>
+    </a>
+</div>
 <br />
 <div class="article-header-small">OVERVIEW</div>
 <div class="article-content">
